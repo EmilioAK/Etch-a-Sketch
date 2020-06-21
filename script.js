698940dcf5511ff7d4d4cfc6648c51a17f9e64fc
EmilioAK <emilio.alvarez96@gmail.com>
2020-06-21 03:53:11 +0200

    Finish assignment, lol

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-const squares = document.querySelectorAll(".box")
+let squareCreator = function () {
+    const square = document.createElement('div');
+    square.classList.add("box")
+    return square;
+}
 
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
+let rowCreator = function () {
+    const row = document.createElement('div');
+    row.classList.add("row");
+    return row;
+}
+
+let gridConstructor = function (squaresPerSide) {
+    const grid = document.querySelector(".grid");
+    
+    //Create squaresPerSide number of rows
+    for (let i = 0; i < squaresPerSide; i++) {
+        const row = rowCreator()
+        
+        //Create squaresPerSide number of squares
+        for (let i = 0; i < squaresPerSide; i ++) {
+            const square = squareCreator();
+            row.appendChild(square);
+        };
+
+        grid.appendChild(row);
+    };
+};
+
+let createTrail = function () {
+    const squares = document.querySelectorAll(".box");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
         square.style.background = "grey";
+        });
     });
+    return squares;
+};
+
+let createGrid = function () {
+    let userGridNumber = window.prompt("Enter squares per side: ");
+    gridConstructor(userGridNumber);
+
+    createTrail();
+};
+
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => {
+    const grid = document.querySelector(".grid");
+    grid.innerHTML = '';
+    createGrid();
 });
+
+
+createGrid();
